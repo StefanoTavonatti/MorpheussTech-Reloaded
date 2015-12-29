21a40fa276fdbb6afb23e43cfc6a9ff948184745
Stefano Tavonatti <stefanotavonatti93@gmail.com>
2015-12-29 21:17:14 +0100

    Materials

---
+++ src/main/java/Morpheuss93/MorpheussTechReloaded/tools/Materials.java
@@ -0,0 +1,13 @@
+package Morpheuss93.MorpheussTechReloaded.tools;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.common.util.EnumHelper;
+
+/**
+ * Created by stefano on 29/12/15.
+ */
+public class Materials {
+    public static Item.ToolMaterial MaterialBronze=  EnumHelper.addToolMaterial("BRONZE", 2, 1561, 8.0F, 2.0F, 10);
+    public static Item.ToolMaterial MaterialBrass=  EnumHelper.addToolMaterial("BRASS", 2, 1024, 7.0F, 2.0F, 10);
+    public static Item.ToolMaterial MaterialSteel=  EnumHelper.addToolMaterial("STEEL", 3, 918, 8.0F, 3.0F, 10);
+}
